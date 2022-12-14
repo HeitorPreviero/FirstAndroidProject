e53167ffbd0007df871db2eaa36bdbdbad0ce0c5
HeitorPreviero <heitor.previero@optimumsupply.com.br>
2022-12-13 22:46:31 -0300

    Enviando dados digitados entre duas telas

--- app/src/main/java/com/example/meuprimeiroaplicativo/ResultActivity.kt
+++ app/src/main/java/com/example/meuprimeiroaplicativo/ResultActivity.kt
@@ -1,11 +1,18 @@
 package com.example.meuprimeiroaplicativo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val nomeDigitado = intent.getStringExtra("NOME_DIGITADO")
+
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+        tvResultado.text = nomeDigitado
     }
 }
