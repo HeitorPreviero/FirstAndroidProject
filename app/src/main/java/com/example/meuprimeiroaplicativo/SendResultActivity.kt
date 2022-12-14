992566f7fd2df60c88e31391e0c04997fdcc5ccb
HeitorPreviero <heitor.previero@optimumsupply.com.br>
2022-12-13 23:48:59 -0300

    finalizado codigos da aula

---
+++ app/src/main/java/com/example/meuprimeiroaplicativo/SendResultActivity.kt
@@ -0,0 +1,43 @@
+package com.example.meuprimeiroaplicativo
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.annotation.StringRes
+
+class SendResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_result)
+
+        val btYes = findViewById<Button>(R.id.btYes)
+        val btNo = findViewById<Button>(R.id.btNo)
+        val btCancel = findViewById<Button>(R.id.btCancel)
+
+        btYes.setOnClickListener {
+            sendResult(R.string.yes)
+        }
+
+        btNo.setOnClickListener {
+            sendResult(R.string.no)
+        }
+
+        btCancel.setOnClickListener {
+            setResult(Activity.RESULT_CANCELED)
+
+            finish()
+        }
+    }
+
+    private fun sendResult(@StringRes stringResId: Int) {
+        val intent = Intent()
+
+        intent.putExtra("RESULT", getString(stringResId))
+
+        setResult(Activity.RESULT_OK, intent)
+
+        finish()
+    }
+}
